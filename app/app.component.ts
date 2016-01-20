5168f2c0964be3497915013378ca8bc96cd022cf
Admir Husovic <mail.admir.husovic@gmail.com>
2016-01-20 11:43:30 +0100

    add admin component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,14 @@ import {CardsComponent} from './cards.component';
 import {MenuComponent} from './menu.component';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Login} from './login/login.component';
+import {Admin} from './admin/admin.component';
 
 @Component({
     selector: 'app',
     templateUrl: 'app/app.component.html',
     directives: [
       ROUTER_DIRECTIVES,
+      Admin,
       CardsComponent,
       MenuComponent
     ]
@@ -16,6 +18,7 @@ import {Login} from './login/login.component';
 
 @RouteConfig([
   {path:'/login', name: 'Login', component: Login},
+  {path:'/admin', name: 'Admin', component: Admin}
 ])
 
 export class AppComponent { }
