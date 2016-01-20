dd1fd22be51cc3b579cd69098058b9955e86a534
Sofie kedvik <sofie.kedvik@gmail.com>
2016-01-20 12:08:55 +0100

    moved info

---
+++ app/cards/cards.component.ts
@@ -0,0 +1,41 @@
+import {Component} from 'angular2/core';
+import {ShortText} from '../pipes/shortText.pipe';
+
+@Component({
+  selector: 'card',
+  template: `
+      <div class="card col-md-3" *ngIf=data.card.article>
+        <img src="{{data.card.images}}">
+        <h3>{{data.card.header}}</h3>
+        <p class="hide">Date: {{data.card.date}}</p>
+        <p><i>{{data.card.text | shortText}}</i></p>
+        <p class="hide">Author: {{data.card.author}} </p>
+        <footer>
+          <h4 class="category {{data.card.category[0]}}">{{data.card.category[0]}}</h4>
+          <strong class="hide">Category: {{data.card.category}}</strong>
+          <button class="read-btn">Read more</button>
+        </footer>
+        <a href="{{data.card.link}}" class="hide">Read this Article</a>
+      </div>
+
+      <div class="card col-md-3"*ngIf=data.card.tutorial>
+        <img src="{{data.card.images}}">
+        <h3>{{data.card.header}}</h3>
+        <p class="hide">Date: {{data.card.date}}</p>
+        <p><i>{{data.card.text | shortText}}</i></p>
+        <p class="hide">Author: {{data.card.author}} </p>
+        <footer>
+          <h4 class="category {{data.card.category[0]}}">{{data.card.category[0]}}</h4>
+          <strong class="hide">Category: {{data.card.category}}</strong>
+          <button class="read-btn">Read more</button>
+        </footer>
+        <a href="{{data.card.link}}" class="hide">Read this Article</a>
+      </div>
+    `,
+    inputs: ['data'],
+    pipes: [ShortText]
+})
+
+export class CardsComponent {
+
+}
