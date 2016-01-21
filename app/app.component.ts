0d0793bcc77bdbd5c3b0676d9ca2e8eea809207a
Admir Husovic <mail.admir.husovic@gmail.com>
2016-01-21 14:42:38 +0100

    add proper router handle

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from 'angular2/core';
+import {Component,provide,View} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
 import {CardsComponent} from './cards/cards.component';
 import {MenuComponent} from './menu/menu.component';
 import {FilterCatComponent} from './filter-cat/filter-cat.component';
@@ -6,24 +7,34 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Login} from './login/login.component';
 import {Admin} from './admin/admin.component';
 import {FetchJsonPipe} from './pipes/fetch-json.pipe';
-
+import {Home} from './home/home.component';
+import {Dashboard} from './dashboard/dashboard.component';
+import {CanActivate, ROUTER_PROVIDERS, Location, LocationStrategy, HashLocationStrategy,RouterLink, RouterOutlet} from 'angular2/router';
+import {Navigation} from './navigation/navigation.component';
 
 @Component({
     selector: 'app',
     templateUrl: 'app/app.component.html',
     directives: [
-      ROUTER_DIRECTIVES,
+      Navigation,
       Admin,
+      Login,
+      Dashboard,
       CardsComponent,
       MenuComponent,
-      FilterCatComponent
+      FilterCatComponent,
+      ROUTER_DIRECTIVES,
+      RouterLink,
+      RouterOutlet
     ],
     pipes: [FetchJsonPipe]
 })
 
 @RouteConfig([
-  {path:'/login', name: 'Login', component: Login},
-  {path:'/admin', name: 'Admin', component: Admin}
+  {path:'/', name: 'Home', component: Home},
+  {path:'/Login', name: 'Login', component: Login},
+  {path:'/Dashboard', name: 'Dashboard', component: Dashboard},
+  {path:'/Admin', name: 'Admin', component: Admin}
 ])
 
 export class AppComponent {
@@ -31,3 +42,7 @@ export class AppComponent {
     cat: 1
   }
  }
+ bootstrap(AppComponent,  [
+     ROUTER_PROVIDERS,
+     provide(LocationStrategy, {useClass: HashLocationStrategy})
+ ]);
