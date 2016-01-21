ea7dd77c7499619494e4fc020db1597427e05fb4
Admir Husovic <mail.admir.husovic@gmail.com>
2016-01-21 14:59:32 +0100

    start from scratch login

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,4 +1,3 @@
-///<reference path='../firebase.d.ts'/>
 import {RouterLink} from 'angular2/router';
 import {Component,View} from 'angular2/core';
 import {NgForm} from 'angular2/common';
@@ -10,69 +9,5 @@ import {NgForm} from 'angular2/common';
 })
 
 export class Login {
-
-  constructor(
-    public firebase: string,
-    public username: string,
-    public password: string,
-    public text: string
-  ){ }
-
-  // var firebase = new Firebase("https://omgsrc.firebaseio.com");
-
-  // function logInputData(username, password){
-  //   console.log(username);
-  // }
-  // hämta input data
-    // verifiera
-    // sätt variabler
-
-    // function typeSome(text){
-    //   console.log(text);
-    // }
-  // firebase.createUser({
-  //   email    : "lookforadmir@gmail.com",
-  //   password : "monkey"
-  // }, function(error, userData) {
-  //   if (error) {
-  //     console.log("Error creating user:", error);
-  //   } else {
-  //     console.log("Successfully created user account with uid:", userData.uid);
-  //   }
-  // });
-
-  // function getUserInput(){
-  //   var username = docuement.querySelector('#username').value;
-  //   var password = docuement.querySelector('#passoword').value;
-  //
-  //   userLogin(username, password);
-  //
-  //   function userLogin(username, password){
-  //     firebase.authWithPassword({
-  //      email    : username,
-  //      password : password
-  //      }, function(error, authData) {
-  //        if (error) {
-  //          console.log("Login Failed!", error);
-  //        } else {
-  //         console.log("Authenticated successfully with payload:", authData);
-  //       }
-  //     });
-  //   }
-  //
-  // }
-
-  // function createAUser(firebase){
-  //   firebase.createUser({
-  //   // email    : … ,
-  //   // password : …
-  //   }, function(error, userData) {
-  //     if (error) {
-  //       console.log("Error creating user:", error);
-  //     } else {
-  //       console.log("Successfully created user account with uid:", userData.uid);
-  //     }
-  //   })
-  // }
 
 }
